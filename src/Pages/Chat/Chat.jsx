62586520afaa2456bd96be456c16df944f4e63ea
David Lucas Flayban <davidflayban2@hotmail.com>
2023-11-15 17:57:28 -0300

    Finalizacao do css e html da page chat

--- src/Pages/Chat/Chat.jsx
+++ src/Pages/Chat/Chat.jsx
@@ -1,31 +1,44 @@
 import styles from "./Chat.module.css"
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 const Chat = () => {
   const [messages, setMessages] = useState([])
   const [inputMessage, setInputMessage] = useState('')
+  const inputRef = useRef(null);
 
   const handleSubmit = (e) => {
     e.preventDefault()
     if (inputMessage) {
-      setMessages([ {text: inputMessage, sent: true},...messages])
+      setMessages([ ...messages, {text: inputMessage, sent: true}])
       setInputMessage('')
     }
- }
-  return (
-    <div>
-      <div className={styles.chatContainer}>
-        <div className={styles.messages}>{messages.map((message, index) => (
-            <div key={index} className={message.sent ? 'message messageSent' : 'message messageReceived'}>
-              {message.text}
-            </div>
-          ))}
-        </div>
-        <form className={styles.inputContainer} onSubmit={handleSubmit}>
-          <input type="text" value={inputMessage} onChange={(e) => setInputMessage(e.target.value)} className={styles.inputField} placeholder="Escreva sua mensagem..."/>
-          <button type="submit" className={styles.sendButton}>Enviar</button>
-        </form>
+  }
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter' && !e.shiftKey) {
+      e.preventDefault();
+      handleSubmit(e);
+    }
+  }
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [])
+
+  return (    
+    <div className={styles.chatContainer}>
+      <div className={styles.messages}>{messages.map((message, index) => (
+          <div key={index} className={message.sent ? styles.messageSent : styles.messageReceived}>
+            {message.text}
+          </div>
+        ))}
       </div>
-    </div>
+      <form className={styles.inputContainer} onSubmit={handleSubmit}>
+        <textarea ref = {inputRef} type="body" value={inputMessage} onChange={(e) => setInputMessage(e.target.value)} onKeyPress={handleKeyPress} className={styles.inputField} placeholder="Escreva sua mensagem..."/>
+        <button type="submit" className={styles.sendButton}>Enviar</button>
+      </form>
+    </div>  
   )
 }
 
