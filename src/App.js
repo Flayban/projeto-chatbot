d32f4ab6922853fd2726a5ca6b0bbb4a35d3036c
David Lucas Flayban <davidflayban2@hotmail.com>
2023-11-15 10:34:45 -0300

    Conectando rota das pages com o app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-
+//Pages
+import BaseInfo from "./Pages/BaseInfo/BaseInfo";
+import Chat from "./Pages/Chat/Chat";
 function App() {
   return (
     <div className="App">
-      Projeto Chatbot
+      <BrowserRouter>
+        <div>
+          <Routes>
+            <Route path='BasedeInformacao' element={<BaseInfo/>}/>
+            <Route path='chatbot' element={<Chat/>}/>
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
-
 export default App;
