8b0fd35b2e9a2374e9091f1687f5cd1b479b6f23
David Lucas Flayban <davidflayban2@hotmail.com>
2023-11-21 15:06:30 -0300

    Adicionando permissões das rotas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
-
+//Hooks
+import { useState, useEffect } from "react"
+import { useAuthentication } from "./Hooks/useAuthentication"
+import { onAuthStateChanged } from "firebase/auth"
+//Context
+import { AuthProvider } from './Context/AuthContext'
 //Pages
 import BaseInfo from "./Pages/BaseInfo/BaseInfo";
 import Chat from "./Pages/Chat/Chat";
@@ -10,21 +15,39 @@ import Register from './Pages/Register/Register';
 //Components
 import Navbar from './Components/Navbar';
 function App() {
+
+  const [user, setUser] = useState(undefined)
+  const { auth } = useAuthentication()
+
+  const loadingUser = user === undefined
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+  }, [auth]);
+
+  if (loadingUser) {
+    return <p>Carregando</p>
+  }
+
   return (
     <div className="App">
-      <BrowserRouter>
-      <Navbar/>
-        <div>
-          <Routes>
-            <Route path='/' element={<Navigate to='/BasedeInformacao'/> }/>
-            <Route path='/BasedeInformacao' element={<BaseInfo/>}/>
-            <Route path='/Chatbot' element={<Chat/>}/>  
-            <Route path='/Sobre' element={<Sobre/>}/> 
-            <Route path='/Login' element={<Login/>}/>
-            <Route path='/Register' element={<Register/>}/>    
-          </Routes>
-        </div>
-      </BrowserRouter>
+      <AuthProvider value = {{user}}>
+        <BrowserRouter>
+        <Navbar/>
+          <div>
+            <Routes>
+              <Route path='/' element={!user? <Navigate to='/Register'/> : <Navigate to='/BasedeInformacao'/> }/>
+              <Route path='/BasedeInformacao' element={ !user? <BaseInfo/> : <Navigate to='/Register'/> }/>
+              <Route path='/Chatbot' element={<Chat/>}/>  
+              <Route path='/Sobre' element={<Sobre/>}/> 
+              <Route path='/Login' element={!user? <Login/> : <Navigate to = '/Chatbot'/>}/>
+              <Route path='/Register' element={!user? <Register/>: <Navigate to = '/Chatbot'/>}/>    
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
