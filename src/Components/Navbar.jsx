db31a4b002094fec4f43ac2d0d2ca554b52f75ae
David Lucas Flayban <davidflayban2@hotmail.com>
2023-11-21 14:56:15 -0300

    Colocando rotas na navbar para login e logout

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -11,17 +11,39 @@ const Navbar = () => {
   return (
     <nav className={styles.navbar}>
         <ul className={styles.links_list}>
-          <li>
-              <NavLink to ="/BasedeInformacao" className={({ isActive }) => (isActive ? styles.active : "")}> Base de Informação </NavLink>
-          </li>
-          <li>
-              <NavLink to ="/Chatbot" className={({ isActive }) => (isActive ? styles.active : "")}> Chat Bot </NavLink>
-          </li>
+          {!user &&(
+            <>                  
+              <li>
+                <NavLink to ="/Chatbot" className={({ isActive }) => (isActive ? styles.active : "")}> Chat Bot </NavLink>
+              </li>
+            </>
+          )}
+          {user &&(
+            <>
+              <li>
+                <NavLink to ="/BasedeInformacao" className={({ isActive }) => (isActive ? styles.active : "")}> Base de Informação </NavLink>
+              </li>  
+            </>
+          )}          
         </ul>
         <ul className={styles.links_list}>
-          <li>
-              <NavLink to ="/Login" className={({ isActive }) => (isActive ? styles.active : "")}> Login </NavLink>
-          </li>
+          {!user &&(
+            <>
+              <li>
+                <NavLink to ="/Register" className={({ isActive }) => (isActive ? styles.active : "")}> Sing up </NavLink>
+              </li>
+              <li>
+                <NavLink to ="/Login" className={({ isActive }) => (isActive ? styles.active : "")}> Log in </NavLink>
+              </li>
+            </>
+          )}         
+          {user &&(
+            <>
+              <li>
+                <button onClick={logout}>Log out</button>
+              </li>
+            </>
+          )}
           <li>
               <NavLink to ="/Sobre" className={({ isActive }) => (isActive ? styles.active : "")}> Sobre </NavLink>
           </li>
