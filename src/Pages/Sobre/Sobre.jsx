8e1c2c46b7302c0260449cee9ea015f095b9be70
David Lucas Flayban <davidflayban2@hotmail.com>
2023-11-24 14:42:37 -0300

    atualizações na page Sobre

--- src/Pages/Sobre/Sobre.jsx
+++ src/Pages/Sobre/Sobre.jsx
@@ -12,9 +12,21 @@ const About = () => {
         <a className={styles.communication} href="https://github.com/Flayban" target="_blank" rel="noopener noreferrer"><ion-icon name="logo-github"></ion-icon></a>
         <a className={styles.communication} href="https://api.whatsapp.com/send/?phone=5587981211940&text&type=phone_number&app_absent=0" target="_blank" rel="noopener noreferrer"><ion-icon name="logo-whatsapp"></ion-icon></a>
         <a className={styles.communication} href="mailto:davidflayban2@hotmail.com, david.flayban@ufape.edu.br" target="_blank" rel="noopener noreferrer"><ion-icon name="mail-outline"></ion-icon></a>
+        <p>
+          <a href="https://drive.google.com/drive/folders/1MdLRwDK_QichW7Iio9PJdsuQKVzBl9qD?usp=sharing" target="_blank" rel="noopener noreferrer"><span class="highlight" >CERTIFICAÇÕES</span></a>
+        </p>
       </div>
       <div className={styles.complement}>     
-        <p className={styles.title}><span class="highlight">Desenvolvedor</span></p>
+        <p className={styles.title}><span class="highlight">Projeto Final da capacitação - Programa Jovens Talentos da eLife Brasil</span></p>
+        <p class={styles.description}>
+          Este projeto tem como o intuito verificar o aprendizado dos discentes da segunda turma do Programa Jovens Talentos da eLife Brasil. Que deve seguir as seguintes caracteristicas:
+          <p>
+            <li>Criar duas paginas em React.js: Uma para Configurar um ChatBot e outra para conversar com o Bot.</li>
+            <li>Implementar um gerador de Chatbot que use a API da OpenAi para gerar as respostas do Chatbot e utilizar o MongoDB para armazenar as configurações do Chatbot.</li>
+            <li>Desenvolver uma apresentação em forma de video, demostrando o funcionamento do projeto e comentando suas principais dificuldades. Video esse disponivel clicando <a href="https://drive.google.com/drive/folders/12FUNPnMqX7FdJAFjY-iYTj13VUNoTir9?usp=sharing" target="_blank" rel="noopener noreferrer"><span class="highlight" >aqui.</span></a></li>
+          </p>
+        </p>
+        <p className={styles.title}><span class="highlight">Desenvolvedor</span></p>        
         <p class={styles.description}>Sou graduando em <span class="highlight">Ciência da Computação</span>. Atualmente dedico-me aos estudos, buscando sempre aprender novas habilidades e ferramentas. Também sou prestativo e gosto de colaborar com outros profissionais e estudantes da área, compartilhando conhecimentos e ideias. Busco participar de projetos para adquirir crescimento e experiência profissional e pessoal. Atuo tanto no <span class="highlight">back-end</span> quanto no <span class="highlight">front-end</span></p>
         <h2 className={styles.skillsSectionTitle}>Habilidades:</h2>
         <div className={styles.skillsContainer}>
